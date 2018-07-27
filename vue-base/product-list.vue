10e239378a16c14981305752164a43003fa45a68
ArvinSpace <yanhui-180@163.com>
2018-07-27 15:22:16 +0800

    move to base directory

---
+++ vue-base/product-list.vue
@@ -0,0 +1,15 @@
+<ul>
+    <li v-for="(product, index) of products" v-bind:key="product.id" role="presentation">
+        <input type="number" v-model.number="product.quantity" v-if="index % 2 === 0"/>
+        <!--<span v-if="index % 2 != 0">{{ product.quantity }}</span> {{ product.name }}-->
+        {{ index % 2 != 0 ? product.quantity : '' }} {{ product.name }}
+        <button @click="product.quantity += 1">Add</button>
+        <button @click="product.quantity -= 1">Reduce</button>
+        <span v-if="product.quantity <= 0"> - OUT OF QUANTITY</span>
+    </li>
+</ul>
+<label>total products:{{ products.length }} - total quantity:{{ totalQuantity }}</label>
+<p>
+    <button @click="products.push({ name: Math.floor(Math.random() * 10 * 100), quantity: Math.floor(Math.random() * 10) })">Add</button>
+    <button @click="products.pop()">Reduce</button>
+</p>
